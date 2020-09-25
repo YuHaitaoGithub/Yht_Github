dee7bc5b9366ec3e0e0c29f64f58d0ba91b4392b
yht <1424634450@qq.com>
2020-09-25 10:38:21 +0800

    冲突解决

--- project1/Project1/源.cpp
+++ project1/Project1/源.cpp
@@ -3,11 +3,7 @@
 #include "stdio.h"
 void main(void)
 {
-<<<<<<< .merge_file_a08400
-	char ch[] = { "hello£¬ÊÀ½ç£¡" };
-=======
-	char ch[] = { "ÄãºÃ£¬word£¡" };
->>>>>>> .merge_file_a16244
+	char ch[] = { "hello£¬word£¡" };
 	printf("%s\n", ch);
 	system("pause");
 }
